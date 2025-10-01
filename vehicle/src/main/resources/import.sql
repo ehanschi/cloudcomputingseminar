001cb3837f0faace449a8744efd84e38446bede2
Mag. Dipl.-Ing. Erich Hanschitz <erich.hanschitz@htl-klu.at>
2025-10-01 14:13:59 +0200

    import.sql

--- vehicle/src/main/resources/import.sql
+++ vehicle/src/main/resources/import.sql
@@ -1,11 +1,2 @@
-insert into vehicle (brand, model)
-values
-    ('Opel', 'Kadett'),
-    ('Opel', 'Insignia'),
-    ('Opel', 'Astra'),
-    ('BMW', 'X3'),
-    ('BMW', 'X5'),
-    ('BMW', 'i7'),
-    ('Tesla', 'Model Y'),
-    ('Tesla', 'Model S')
-;
+
+
